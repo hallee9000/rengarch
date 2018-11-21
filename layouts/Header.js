21d92666f9c176f6338faf8000decca8180bd138
Jun <857098475@qq.com>
2018-11-21 22:12:54 +0800

    基本样式

--- layouts/Header.js
+++ layouts/Header.js
@@ -1,15 +1,15 @@
 import "../assets/base.less"
 import "./header.less"
 export default props => 
-	<header className="header">
+	<header className="header container">
 		<nav className="header-nav">
 			<a className="nav-link nav-logo">
 				<img src="/static/logo.png"/>
 			</a>
 			<span className="nav-stretch"/>
-			<a className="nav-link">Projects</a>
-			<a className="nav-link">Office</a>
-			<a className="nav-link">Art</a>
-			<a className="nav-link">Contact</a>
+			<a className="nav-link" href="/projects">Projects</a>
+			<a className="nav-link" href="/office">Office</a>
+			<a className="nav-link" href="/art">Art</a>
+			<a className="nav-link" href="/contact">Contact</a>
 		</nav>
 	</header>
