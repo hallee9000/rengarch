1df2f3f540c192810f63eccdd60bb7861ca010d9
Jun <857098475@qq.com>
2019-06-21 09:23:29 +0800

    修改地址信息

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -3,8 +3,7 @@ import "./contact.less"
 export default () =>
 	<div className="contact">
 		<div className="container">
-			<p>北京·东直门·华普花园</p>
-			<p>电话: 15673114479</p>
+			<p>中国·北京</p>
 			<p>电邮: info@rengarch.com</p>
 			<img src="/static/qrcode.jpg"/>
 		</div>
