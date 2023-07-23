1f3dde19fc161faec873fe02027b5e41611ca45a
leadream <leadream4@gmail.com>
2023-07-23 16:55:00 +0800

    framework

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
-const path = require('path')
-const fs = require('fs')
-const appDirectory = fs.realpathSync(process.cwd())
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-const resolveApp = relativePath => path.resolve(appDirectory, relativePath)
-// next.config.js
-const withLess = require('@zeit/next-less')
-const withCss = require('@zeit/next-css')
-
-module.exports = withCss(withLess({
-	webpack(config) {
-		config.resolve.alias.layouts = resolveApp('layouts')
-		config.resolve.alias.components = resolveApp('components')
-		config.resolve.alias.assets = resolveApp('assets')
-		config.resolve.alias.utils = resolveApp('utils')
-		return config
-	}
-}))
+module.exports = nextConfig
---
+++ old/README.md
@@ -0,0 +1,21 @@
+# rengarch
+
+## 开发
+```sh
+yarn dev
+```
+
+## 构建
+```sh
+yarn build
+```
+
+## 生成静态文件
+```sh
+yarn export
+```
+
+## 发布到 CODING Pages
+```sh
+./deploy.sh
+```
